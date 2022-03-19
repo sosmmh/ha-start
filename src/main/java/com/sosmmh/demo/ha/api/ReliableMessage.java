f8165dcb40d3265f71ecdd925955beaed94b8a34
sosmmmh <5335707+sosmmmh@user.noreply.gitee.com>
2022-03-19 15:46:56 +0800

    first commit

---
+++ src/main/java/com/sosmmh/demo/ha/api/ReliableMessage.java
@@ -0,0 +1,17 @@
+package com.sosmmh.demo.ha.api;
+
+import lombok.Data;
+
+/**
+ * @author Lixh
+ */
+@Data
+public class ReliableMessage {
+    private Long id;
+    private String msgId;
+    private String topic;
+    private String message;
+    private Integer status;
+    private Integer retryCount;
+    private Long nextExcTime;
+}
---
+++ src/main/java/com/sosmmh/demo/ha/enums/MessageStatus.java
@@ -0,0 +1,9 @@
+package com.sosmmh.demo.ha.enums;
+
+/**
+ * @author Lixh
+ */
+public class MessageStatus {
+    public static final Integer SENDING = 1;
+    public static final Integer FAILURE = 2;
+}
